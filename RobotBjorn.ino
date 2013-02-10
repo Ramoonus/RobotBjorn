131bd6a7188fe143fb946e98f24dac08b52695c8
Ramon van Belzen <github@ramoonus.nl>
2013-02-10 17:21:42 +0100

    Pushing version 1.3

--- RobotBjorn.ino
+++ RobotBjorn.ino
@@ -1,10 +1,24 @@
-#define EngineSpeed 192 
+const int EngineSpeed = 192;
 // Recommended: 192 for 5x AA
 // Recommended 96 for 1x 9V
 // Recommended: 64 over USB
 // Min: 0
 // Max: 255
 
+// Sensors
+const char SensorLeftPort    = A5;
+const char SensorMiddlePort  = A5;
+const char SensorRightPort  = A5;
+
+// Piezo Speaker
+const char HornPort = A5; 
+
+// Lights (using shift register)
+const int latchPin = 0; //5
+const int clockPin = 0; //6
+const int dataPin = 0; //4
+
+// Startup Procedure
 void setup() {
   // Open Serial Port
   Serial.begin(9600);  
@@ -20,34 +34,38 @@ void setup() {
   pinMode(13, OUTPUT); //Initiates Motor Channel B pin
   pinMode(8, OUTPUT);  //Initiates Brake Channel B pin
 
+  // Lights (using shift register)
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+  
+  // Turn on Headlights
+  HeadLights();
+  
+  // Ending pin configuration and other startup routines
+  Serial.println('Startup procedure completed');
+
   // Brake on start
   Brake(); 
-  Serial.println('Startup procedure completed');
-  Serial.println('Brake initialized');
+  Serial.println('Startup Brake initialized');
 }
 
+// Execute Procedure
 void loop(){
   // do something
+
+  // Demo
+  Test();
 }
 void Test() {
-  // Short test script
+  // Forward
   DriveForward(); 
+  // Wait
   delay(2500); 
+  // Backward
   Brake(); 
+  // Wait
   delay(2500);
-}
-
-void TestSensor() {
-  if (!SensorMiddle() ) { 
-    DriveForward(); 
-    Serial.println("Go with the flow!" );  
-  }
-  else { 
-    Brake(); 
-    Serial.println("Emergency brake");  
-  }
-
-  delay(1000);
 }
 
 // Brake functions
@@ -60,30 +78,35 @@ void BrakeRight() {
 void Brake() {
   BrakeLeft(); 
   BrakeRight(); 
+  BrakeLight();
 }
+
 // Drive in reverse function
 void ReverseLeft() {
   //Motor B backward @ full speed
   digitalWrite(13, LOW);  //Establishes backward direction of Channel B
   digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  analogWrite(11, EngineSpeed);    //Spins the motor on Channel B at full speed
+  analogWrite(11, EngineSpeed);    //Spins the motor on Channel B 
 }
 void ReverseRight() {
   //Motor A backwards @ full speed
   digitalWrite(12, LOW); //Establishes forward direction of Channel A
   digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-  analogWrite(3, EngineSpeed);   //Spins the motor on Channel A at full speed
+  analogWrite(3, EngineSpeed);   //Spins the motor on Channel A 
 }
 void DriveReverse() {
+  ReverseLight();
   ReverseLeft();
   ReverseRight();
+  ReverseLightOff();
 }
+
 // Drive specific way function
 void DriveLeft() {
   //Motor B forward @ full speed
   digitalWrite(13, HIGH); //Establishes forward direction of Channel B
   digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  analogWrite(11, EngineSpeed);   //Spins the motor on Channel B at full speed
+  analogWrite(11, EngineSpeed);   //Spins the motor on Channel B 
 }
 void DriveRight(){
   //Motor A forward @ full speed
@@ -97,45 +120,37 @@ void DriveForward() {
 }
 // Turn Left
 void TurnLeft() {
+  LeftLight(); 
   ReverseLeft();
   DriveRight();
+  LeftLightOff();
 }
 // Turn Right
 void TurnRight() {
+  RightLight(); 
   ReverseRight();
-  DriveLeft();
+  RightLightOff();
 }
-// Head Lights 
-void HeadLights() {
-}
-// Brake Light
-void BrakeLight(){
-}
-// Turn Left Light
-void LeftLight() {
-}
-// Turn Right Light
-void RightLight() {
-}
+
 // Sensor Left
 int SensorLeft() {
-  if( analogRead(A5) > 512 ) { 
+  if( analogRead(SensorLeftPort) > 512 ) { 
     return 1; 
-  } // please select left port
+  }
   else return 0; 
 }
 // Sensor Middle
 int SensorMiddle() {
-  if( analogRead(A5) > 512 ) { 
+  if( analogRead(SensorMiddlePort) > 512 ) { 
     return 1; 
-  } // please select middle port
+  }
   else return 0; 
 }
 // Sensor Right
 int SensorRight() {
-  if( analogRead(A5) > 512 ) { 
+  if( analogRead(SensorRightPort) > 512 ) { 
     return 1; 
-  } // please select right port
+  } 
   else return 0; 
 }
 // Current Sensing Channel A
@@ -152,27 +167,58 @@ int CurrentSenseB() {
 }
 // Sensor Left Debug
 void SensorLeftDebug() {
-  Serial.println("Reading on A5");
-  Serial.println(analogRead(A5) );
+  Serial.println("Reading on ");
+  Serial.println( SensorLeftPort );
+  Serial.println(analogRead(SensorLeftPort) );
   Serial.println("Reading sensor left function");
   Serial.println(SensorLeft() );
-  delay(1000);
 }
 // Sensor Middle Debug
 void SensorMiddleDebug() {
-  Serial.println("Reading on A5");
-  Serial.println(analogRead(A5) );
+  Serial.println("Reading on ");
+  Serial.println( SensorMiddlePort );
+  Serial.println(analogRead(SensorMiddlePort) );
   Serial.println("Reading sensor middle function");
   Serial.println(SensorMiddle() );
-  delay(1000);
 }
 // Sensor Right Debug
 void SensorRightDebug() {
-  Serial.println("Reading on A5");
-  Serial.println(analogRead(A5) );
+  Serial.println("Reading on ");
+  Serial.println( SensorRightPort );
+  Serial.println(analogRead(SensorRightPort) );
   Serial.println("Reading sensor right function");
   Serial.println(SensorRight() );
-  delay(1000);
 }
-
-
+// Horn / Beeper 
+void Horn() {
+  //tone(pin, frequency, duration)  
+  tone(HornPort, 262, 1500);
+  // Frequency 262 = C4
+}
+// Head Lights 
+void HeadLights() {
+  // Enable Headlight left
+  // Enable Headlight right
+}
+// Brake Light
+void BrakeLight(){
+  // Enable Breaklight left
+  // Enable Breaklight right
+}
+// Turn Left Light
+void LeftLight() {
+}
+// Turn Left Light Off
+void LeftLightOff() {
+}
+// Turn Right Light
+void RightLight() {
+}
+// Turn Right Light
+void RightLightOff() {
+}
+// White lights when in reverse
+void ReverseLight(){
+}
+void ReverseLightOff(){
+}
--- readme.md
+++ readme.md
@@ -2,34 +2,40 @@
 By [Ramoonus](http://www.ramoonus.nl)
 
 RobotBjorn is a Arduino based 4 wheel drive robot. 
-It uses basic Arduino Shields and open source firmware
-The Robot costs approx. 100 EURO to make.
+It uses basic Arduino Shields and open source firmware (writting in C++, Wiring and Processing)
+The Robot costs approx. 100 EURO to build and requires little expertise.
 
 Compatibility with third-party shields and platforms is described in the [Compatibility](compatibility.md) document.
 
-Future Development is specified in a [Todo](todo.md)
+Future development is specified in a [Todo](todo.md)
 
 # Donate #
 The Robot itself costs approx. 100 Euro but modifications and tutorial material costs much more.
 Please sponsor this project by donating via PayPal using the link: 
-[Donate](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RZ2HK2NADF4DC)
+[Donate](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RZ2HK2NADF4DC "Donate through PayPal")
 
 ## Software Toolkit ##
 I used the following software for the development:
 
-* [Arduino IDE](http://arduino.cc/en/Main/Software) 1.0.4 and 1.5.2
-* [MarkdownPad](http://markdownpad.com/) (for Windows)
-* [Fritzing](http://fritzing.org/) 0.7.11b
+* [Arduino IDE](http://arduino.cc/en/Main/Software "Download Arduino IDE") 1.0.4 and 1.5.2
+* [MarkdownPad](http://markdownpad.com/ "Download MarkdownPad") (for Windows)
+* [Fritzing](http://fritzing.org/ "Download Fritzing") 0.7.11b
 * [Processing](http://processing.org/download/ "Download Processing") 2.0 Beta 7
 
 ## Release Notes ##
-### 1.3 (current dev version) ###
+### 1.3 - 10-2-2013 ###
 * Improved Documentation (see [Relative links in markup files - GitHub blog](https://github.com/blog/1395-relative-links-in-markup-files) )
 * Primary development platform is now Arduino 1.5
 * Started development of Processing client
+* Replaced #define with const (improved code styling)
+* Rewritten sensor reading, now with dynamic port mapping (SensorLeftPort etc)
+* Added function Horn (claxon, beep) function and port (HornPort)
+* Enabled lights for turning left and right
+* Reverse Lights
+* Brake Lights
 
-### 1.2 ###
-* Engine Current Reading in mA for both channels A and B
+### 1.2 12-1-2013 ###
+* Engine Current Reading in mA for both channels A and B (hardware mod)
 
 ### 1.1 - 30-12-2012 ###
 * Updated Schematics
@@ -39,8 +45,8 @@ I used the following software for the development:
 * Readme
 
 ### 1.0 - 22-12-2012 ###
-* Created drive and brake functions to simplify controls
+* Created drive and brake functions to simplify controls, aswell as per channel control
 * Test function to test drive and brake functions
-* Documented the code
-* Schematics
+* Documented the code (see source)
+* Drawing Schematics (using Fritzing)
 
