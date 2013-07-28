37a789173c1bc77ee3ed718e28ece926d169ea58
Ramon van Belzen <github@ramoonus.nl>
2013-07-28 11:53:40 +0200

    Various code improvements
    Implemented pin config for motors, added debug var, test does dance

--- RobotBjorn.ino
+++ RobotBjorn.ino
@@ -1,41 +1,43 @@
 // Sensors
-const char SensorLeftPort    = A5;
-const char SensorMiddlePort  = A5;
-const char SensorRightPort  = A5;
-
-// Piezo Speaker
-const char HornPort = A5; 
+const char SensorLeftPin    = A5;
+const char SensorMiddlePin  = A5;
+const char SensorRightPin  = A5;
 
 // Motor
 int EngineSpeed = 128;
-// Recommended: 192 for 5x AA
-// Recommended 96 for 1x 9V
-// Recommended: 64 over USB
-// Min: 0
-// Max: 255
+// Recommended: 192 for 5x AA, 96 for 1x 9V 64 over USB
+// Min: 0  Max: 255
+// Channel A = right engine
+// Channel B = left engine
+const char MotorChannelApin = 12;
+const char MotorBrakeApin = 9;
+const char MotorChannelAPWMpin = 3;
+const char MotorChannelBpin = 13;
+const char BrakeBpin = 8;
+const char MotorChannelBPWMpin = 11;
+// Horn
+const char HornPin = 2; 
+
+// Debug
+const boolean Debug = false;
 
 // Startup Procedure
 void setup() {
-  // Open Serial Port
+  // Open Serial Pin
   Serial.begin(9600);  
 
-  // Channel A = right engine
-  // Channel B = left engine
-
   //Setup Channel A
-  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
+  pinMode(MotorChannelApin, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(MotorBrakeApin, OUTPUT); //Initiates Brake Channel A pin
 
   //Setup Channel B
-  pinMode(13, OUTPUT); //Initiates Motor Channel B pin
-  pinMode(8, OUTPUT);  //Initiates Brake Channel B pin
-
+  pinMode(MotorChannelBpin, OUTPUT); //Initiates Motor Channel B pin
+  pinMode(BrakeBpin, OUTPUT);  //Initiates Brake Channel B pin
 
   // Sensor
-  pinMode(SensorLeftPort, INPUT);
-  pinMode(SensorMiddlePort, INPUT);
-  pinMode(SensorRightPort, INPUT);
-
+  pinMode(SensorLeftPin, INPUT);
+  pinMode(SensorMiddlePin, INPUT);
+  pinMode(SensorRightPin, INPUT);
 
   // Turn on Headlights
   HeadLights();
@@ -51,12 +53,10 @@ void setup() {
   // Ending pin configuration and other startup routines
   Serial.println('Startup procedure completed');
 
-
 }
 
 // Execute Procedure
 void loop(){
-  // Gearbox 
 
   //Test
   //test();
@@ -65,18 +65,20 @@ void loop(){
   KeyboardControl();
 
   // AutoPilot
-  //AutoPilot(); 
-  //delay(500);
-
+  //AutoPilot(); delay(500);
 }
-void Test() {
-  // Forward
-  DriveForward(); 
-  // Wait
+// Test Function
+void Test() { 
+  // Dance
+  DriveForward();
   delay(2500); 
-  // Backward
-  Brake(); 
-  // Wait
+  DriveReverse(); 
+  delay(2500);
+  TurnLeft();
+  delay(2500);
+  TurnRight();
+  delay(2500);
+  Horn();   Horn();   Horn(); 
   delay(2500);
 }
 
@@ -176,10 +178,10 @@ void KeyboardControl(){
         Serial.println("Done");
         break;
 
-      case 'p'://P for AutoPilot
-        Serial.println("Enabling AutoPilot");
-        AutoPilot();
-                Serial.println("Done");
+      case 'h'://Horn
+        Serial.println("Feeling horny!");
+        Horn();
+
         break;
       }
 
@@ -192,45 +194,45 @@ void KeyboardControl(){
 void Gearbox( int gear) {
   // Depends motor PWM by 'gear'
   int  EngineSpeed = map(gear, 0, 9, 0, 255);
-  Serial.print("Gear: ");
-  Serial.println(gear);
-  Serial.print("PWM Speed: ");
-  Serial.println(EngineSpeed);
-  Serial.print("Percentage power: ");
-  Serial.print( EngineSpeed /255 );
-  Serial.println("%");
+  if(Debug) {
+    Serial.print("Gear: ");
+    Serial.println(gear);
+    Serial.print("PWM Speed: ");
+    Serial.println(EngineSpeed);
+    Serial.print("Percentage power: ");
+    Serial.print( EngineSpeed /255 );
+    Serial.println("%"); 
+  }
 }
 
 
 // Brake functions
 void BrakeLeft() {
-  digitalWrite(8, HIGH);  //Engage the Brake for Channel B
+  digitalWrite(BrakeBpin, HIGH);  //Engage the Brake for Channel B
 }
 void BrakeRight() {
-  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+  digitalWrite(MotorBrakeApin, HIGH);  //Engage the Brake for Channel A
 }
 void Brake() {
+  // Brake
   BrakeLeft(); 
   BrakeRight(); 
+  // Brakel ights
   BrakeLight();
 }
-// Alias stop as brake
-void stop() { 
-  Brake; 
-} 
 
 // Drive in reverse function
 void ReverseLeft() {
   //Motor B backward @ full speed
-  digitalWrite(13, LOW);  //Establishes backward direction of Channel B
-  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  analogWrite(11, EngineSpeed);    //Spins the motor on Channel B 
+  digitalWrite(MotorChannelBpin, LOW);  //Establishes backward direction of Channel B
+  digitalWrite(BrakeBpin, LOW);   //Disengage the Brake for Channel B
+  analogWrite(MotorChannelBPWMpin, EngineSpeed);    //Spins the motor on Channel B 
 }
 void ReverseRight() {
   //Motor A backwards @ full speed
-  digitalWrite(12, LOW); //Establishes forward direction of Channel A
-  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-  analogWrite(3, EngineSpeed);   //Spins the motor on Channel A 
+  digitalWrite(MotorChannelApin, LOW); //Establishes forward direction of Channel A
+  digitalWrite(MotorBrakeApin, LOW);   //Disengage the Brake for Channel A
+  analogWrite(MotorChannelAPWMpin, EngineSpeed);   //Spins the motor on Channel A 
 }
 void DriveReverse() {
   ReverseLight();
@@ -242,15 +244,15 @@ void DriveReverse() {
 // Drive specific way function
 void DriveLeft() {
   //Motor B forward @ full speed
-  digitalWrite(13, HIGH); //Establishes forward direction of Channel B
-  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  analogWrite(11, EngineSpeed);   //Spins the motor on Channel B 
+  digitalWrite(MotorChannelBpin, HIGH); //Establishes forward direction of Channel B
+  digitalWrite(BrakeBpin, LOW);   //Disengage the Brake for Channel B
+  analogWrite(MotorChannelBPWMpin, EngineSpeed);   //Spins the motor on Channel B 
 }
 void DriveRight(){
   //Motor A forward @ full speed
-  digitalWrite(12, HIGH);  //Establishes forward direction of Channel A
-  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-  analogWrite(3, EngineSpeed);    //Spins the motor on Channel A at half speed
+  digitalWrite(MotorChannelApin, HIGH);  //Establishes forward direction of Channel A
+  digitalWrite(MotorBrakeApin, LOW);   //Disengage the Brake for Channel A
+  analogWrite(MotorChannelAPWMpin, EngineSpeed);    //Spins the motor on Channel A at half speed
 }
 void DriveForward() {
   DriveLeft();
@@ -273,17 +275,17 @@ void TurnRight() {
 
 // Sensor Left
 int SensorLeft() {
-  if(digitalRead(SensorLeftPort) == HIGH) return 1;
+  if(digitalRead(SensorLeftPin) == HIGH) return 1;
   else  return 0; 
 }
 // Sensor Middle
 int SensorMiddle() {
-  if(digitalRead(SensorMiddlePort) == HIGH) return 1;
+  if(digitalRead(SensorMiddlePin) == HIGH) return 1;
   else  return 0; 
 }
 // Sensor Right
 int SensorRight() {
-  if(digitalRead(SensorRightPort) == HIGH) return 1;
+  if(digitalRead(SensorRightPin) == HIGH) return 1;
   else  return 0; 
 }
 // Current Sensing Channel A
@@ -301,31 +303,31 @@ int CurrentSenseB() {
 // Sensor Left Debug
 void SensorLeftDebug() {
   Serial.println("Reading on ");
-  Serial.println( SensorLeftPort );
-  Serial.println(analogRead(SensorLeftPort) );
+  Serial.println( SensorLeftPin );
+  Serial.println(digitalRead(SensorLeftPin) );
   Serial.println("Reading sensor left function");
   Serial.println(SensorLeft() );
 }
 // Sensor Middle Debug
 void SensorMiddleDebug() {
   Serial.println("Reading on ");
-  Serial.println( SensorMiddlePort );
-  Serial.println(analogRead(SensorMiddlePort) );
+  Serial.println( SensorMiddlePin );
+  Serial.println(digitalRead(SensorMiddlePin) );
   Serial.println("Reading sensor middle function");
   Serial.println(SensorMiddle() );
 }
 // Sensor Right Debug
 void SensorRightDebug() {
   Serial.println("Reading on ");
-  Serial.println( SensorRightPort );
-  Serial.println(analogRead(SensorRightPort) );
+  Serial.println( SensorRightPin );
+  Serial.println(digitalRead(SensorRightPin) );
   Serial.println("Reading sensor right function");
   Serial.println(SensorRight() );
 }
 // Horn / Beeper 
 void Horn() {
   //tone(pin, frequency, duration)  
-  //tone(HornPort, 262, 1500);
+  tone(HornPin, 262, 1500);
   // Frequency 262 = C4
 }
 // Head Lights 
@@ -335,6 +337,8 @@ void HeadLights() {
 }
 // Tail Lights
 void TailLights() {
+  // Enable Taillight Left
+    // Enable Taillight Right
 }
 // Brake Light
 void BrakeLight(){
@@ -358,9 +362,4 @@ void ReverseLight(){
 }
 void ReverseLightOff(){
 }
-
-
-
-
-
 
